a2a9cde732e7f240219b3a0b90215ebf8ae30c66
Amith Chillamcherla <amith.chillamcherla@elgin.com>
2025-03-19 17:56:49 +0000

    shadcn setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Link from "next/link";
+import { homePath, ticketsPath } from "@/paths";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,7 +29,21 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <nav className="flex justify-between px-5 py-2.5 border-b w-full bg-background/95 backdrop-blur fixed left-0 right-0 top-0 z-20 supports-backdrop-blur:bg-background/60">
+          <div>
+            <Link href={homePath()} className="text-lg font-bold">
+              Home
+            </Link>
+          </div>
+          <div>
+            <Link href={ticketsPath()} className="text-sm underline">
+              Tickets
+            </Link>
+          </div>
+        </nav>
+        <main className="py-24 px-8 min-h-screen flex-1 overflow-y-auto overflow-x-hidden bg-secondary/20 flex flex-col">
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,19 @@ import { ticketsPath } from "@/paths";
 
 export default function Home() {
   return (
-    <div>
-      <h2 className="text-lg">Home Page</h2>
-      <Link href={ticketsPath()}>Go to tickects page</Link>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="underline">
+          Go to Tickects
+        </Link>
+      </div>
     </div>
   );
 }
